aef9402d2f24ef6fefa0c9281df7269a0ca2dce5
KaitLand12 <kmlandrove@gmail.com>
2025-05-06 19:29:11 -0400

    Update landing page

---
+++ rotate_words.js
@@ -0,0 +1,42 @@
+const words = ["I'M A WEB DEVELOPER", "I'M A CREATIVE CODER", "I'M A CREATIVE TECHNOLOGIST"];
+let wordIndex = 0;
+const wordElement = document.getElementById("title");
+
+function createLetters(word) {
+  wordElement.innerHTML = "";
+  const letters = word.split("").map((char, index) => {
+    const letterElement = document.createElement("span");
+    letterElement.classList.add("letter");
+    letterElement.classList.add("green");
+    letterElement.style.left = `${index * 20}px`;
+    letterElement.textContent = char;
+    return letterElement;
+  });
+  letters.forEach(letter => wordElement.appendChild(letter));
+  return letters;
+}
+
+function animateWordChange(newWord) {
+  const oldLetters = Array.from(wordElement.children);
+  const newLetters = createLetters(newWord);
+
+  oldLetters.forEach((letter, index) => {
+    letter.classList.add("out");
+    setTimeout(() => {
+      letter.remove();
+    }, 500);
+  });
+
+  newLetters.forEach((letter, index) => {
+    letter.classList.add("in");
+  });
+}
+
+function changeWord() {
+  const nextWord = words[wordIndex];
+  animateWordChange(nextWord);
+  wordIndex = (wordIndex + 1) % words.length;
+}
+
+createLetters(words[0]);
+setInterval(changeWord, 2000);
